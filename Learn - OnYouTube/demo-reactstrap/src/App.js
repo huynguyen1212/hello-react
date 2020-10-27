8708606165f8bf9c737236792e472f5a0c3561d6
HuyNguyen <huynguyen1212.com>
2020-10-27 09:29:05 +0700

    something more

---
+++ Learn - OnYouTube/demo-reactstrap/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "reactstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Button color="primary">primary</Button>{" "}
+      <Button color="secondary">secondary</Button>{" "}
+      <Button color="success">success</Button>{" "}
+      <Button color="info">info</Button>{" "}
+      <Button color="warning">warning</Button>{" "}
+      <Button color="danger">danger</Button> <Button color="link">link</Button>
+    </div>
+  );
+}
+
+export default App;
